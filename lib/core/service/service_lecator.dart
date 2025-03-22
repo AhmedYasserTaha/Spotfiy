fe749458d1f7c2ee1ef5f764384e2dc3f16b291c
AhmedYasserTaha <ayt614614@gmail.com>
2025-03-22 23:31:09 +0200

    create serviceLecator file

---
+++ lib/core/service/service_lecator.dart
@@ -0,0 +1,13 @@
+import 'package:get_it/get_it.dart';
+import 'package:shopy_app/data/repoImpl/auth/auth_repo_impl.dart';
+import 'package:shopy_app/data/sources/auth/auth_firebase_srevice.dart';
+import 'package:shopy_app/domain/repo/auth/auth_repo.dart';
+
+final serviceLecator = GetIt.instance;
+
+Future<void> initializeDependance() async {
+  serviceLecator.registerSingleton<AuthFirebaseSrevice>(
+    AuthFirebaseSreviceImmpl(),
+  );
+  serviceLecator.registerSingleton<AuthRepo>(AuthRepoImpl());
+}
---
+++ lib/data/repoImpl/auth/auth_repo_impl.dart
@@ -0,0 +1,15 @@
+import 'package:shopy_app/domain/repo/auth/auth_repo.dart';
+
+class AuthRepoImpl extends AuthRepo {
+  @override
+  Future<void> signIn() {
+    // TODO: implement signIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signUp() {
+    // TODO: implement signUp
+    throw UnimplementedError();
+  }
+}
