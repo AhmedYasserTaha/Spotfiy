55cd7561adaa376bc827086cee8fb31a828f4dd7
AhmedYasserTaha <ayt614614@gmail.com>
2025-03-22 23:02:08 +0200

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:shopy_app/core/configs/themes/app_theme.dart';
+import 'package:shopy_app/firebase_options.dart';
 import 'package:shopy_app/presentation/choose_mode/cubit/theme_cubit.dart';
 import 'package:shopy_app/presentation/splash/page/splash_view.dart';
 
@@ -15,6 +17,8 @@ Future<void> main() async {
             ? HydratedStorageDirectory.web
             : HydratedStorageDirectory((await getTemporaryDirectory()).path),
   );
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
